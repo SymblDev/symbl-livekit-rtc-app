7caef43fd3e0263f086750b48a97cf39a6b6a105
vishalsharma <vishal.sharma@symbl.ai>
2022-02-11 15:05:30 +0530

    Fixed compilation errors

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,8 @@ const App = () => {
         <div className="container">
             <Router>
                 <Routes>
-                    <Route path="/room">
-                        <RoomPage/>
-                    </Route>
-                    <Route path="/">
-                        <PreJoinPage/>
-                    </Route>
+                    <Route path="/" element={<PreJoinPage/>}/>
+                    <Route path="/room" element={<RoomPage/>}/>
                 </Routes>
             </Router>
         </div>
